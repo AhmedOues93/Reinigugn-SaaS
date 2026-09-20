9aa76ed4bf8f2d1710996bd270605c9f4f38b907
Ahmed-93 <164331146+AhmedOues93@users.noreply.github.com>
2026-09-21 00:29:49 +0200

    Rename invitation cookie to ReinPlan

--- apps/web/lib/invitations.ts
+++ apps/web/lib/invitations.ts
@@ -1,6 +1,6 @@
 import { createHash, randomBytes } from 'crypto';
 
-export const invitationCookieName = 'sauberwerk_invitation';
+export const invitationCookieName = 'reinplan_invitation';
 
 export type InvitationPreview = {
   email: string;
